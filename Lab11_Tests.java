d6d3e75a84166592a89c7fd8b96dde592f84ead4
RoryM0 <rory5827@outlook.com>
2026-04-15 02:09:13 -0400

    test2 and 3 added

--- Lab11_Tests.java
+++ Lab11_Tests.java
@@ -41,13 +41,21 @@ public class Lab11_Tests {
         Lab11_Thread threadA = new Lab11_Thread("A2", 500);
         Lab11_Thread threadB = new Lab11_Thread("B2", 500);
 
+        // Reset shared static list so other tests don't bleed 
+        threadA.setData(new ArrayList<String>());
+
         threadA.start();
         threadB.start();
+
         try {
-            Thread.sleep(500); 
-        } catch (Exception e){
+            Thread.sleep(500);
+        } catch (Exception e) {
             e.printStackTrace();
         }
+
+        // Each thread sleeps 50ms per entry, so after 500ms each should have added ~10
+        // Combined they should have at least 10 entries
+        assertTrue(threadA.getData().size() >= 10);
 
     }
 
@@ -60,14 +68,31 @@ public class Lab11_Tests {
         Lab11_Thread threadA = new Lab11_Thread("A3", 10);
         Lab11_Thread threadB = new Lab11_Thread("B3", 10);
 
+        // Reset shared static list so other tests don't bleed in
+        threadA.setData(new ArrayList<String>());
+
         threadA.start();
-        
+
         try {
             threadA.join();
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        
+
+        // first 10 entries must all belong to A3
+        for (int i = 0; i < 10; i++) {
+            assertTrue(threadA.getData().get(i).startsWith("A3"));
+        }
+
         threadB.start();
+
+        try {
+            threadB.join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // After B finishes, total should be 20
+        assertEquals(20, threadA.getData().size());
     }
 }
